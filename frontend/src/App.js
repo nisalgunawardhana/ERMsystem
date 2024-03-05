1ce6ca71df2f2003d9641764021f0bbf7c567d1f
IT22251596 <IT22251596@my.sliit.lk>
2024-03-05 19:31:14 +0530

    other expenses create retrieve

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import AddOther from './components/AddOther';
+import AllOther from './components/AllOther';
+import {BrowserRouter,Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <div>
+      <Header/>
+      <br></br>
+      <br></br><br></br>
+      <br></br>
+      <Routes>
+        <Route path="/add" element={<AddOther/>}/>
+        <Route path="/" element={<AllOther/>}/>
+      </Routes>
+    
+  </div>
+  </BrowserRouter>
+    
   );
 }
 
