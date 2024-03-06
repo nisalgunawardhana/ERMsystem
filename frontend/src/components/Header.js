c63528ef7129e0e169ea06b1c706e8b8929cd65c
IT22251596 <IT22251596@my.sliit.lk>
2024-03-06 08:31:26 +0530

    package.json update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,11 +4,12 @@ function Header() {
     return (
       <nav class="navbar bg-body-tertiary fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Offcanvas navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+    <a class="navbar-brand" href="#">Offcanvas navbar</a>
+    
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -16,10 +17,10 @@ function Header() {
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="/" href="/">Home</a>
+            <a class="nav-link {{#if activeTab 'home'}}" aria-current="/" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/add">OtherExpenses</a>
+            <a class="nav-link {{#if activeTab 'add'}}" href="/add">OtherExpenses</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
