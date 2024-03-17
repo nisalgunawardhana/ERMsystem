ccece658d1ceecca23d7a7ca1dd76785edc930a5
Nisal Gunawardhana <it22132550@my.sliit.lk>
2024-03-18 01:12:22 +0530

    errofix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,20 @@
-
 import './App.css';
 import Header from './components/Header';
-import Footer from './components/Footer';
 import Home from './components/Home';
 import AddOther from './components/AddOther';
 import AllOther from './components/AllOther';
 import Bill from './components/bill';
 import CreateBill from './components/createBill';
-import UpdateOther from './components/updateOther';
+import UpdateOther from './components/updateBill';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< Updated upstream
-import AddProfit from './components/AddProfit';
-import FinanceDash from './components/financeDash';
-import ProfitDetails from './components/viewProfit';import UpdateBill from './components/updateBill';
-import BillPreview from './components/billPreview';
-=======
 import UpdateBill from './components/updateBill';
-import BillPreview from './components/billPreview'
->>>>>>> Stashed changes
+import BillPreview from './components/billPreview';
 
 
 function App() {
   return (
-    <div>
     <BrowserRouter>
-    
+    <div>
       <Header/>
       <br></br>
       <br></br><br></br>
@@ -32,25 +22,18 @@ function App() {
       
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/profit/:month" element={<AddProfit/>}/>
         <Route path="/otherExpense/add" element={<AddOther/>}/>
         <Route path="/otherExpense" element={<AllOther/>}/>
         <Route path="/bill" element={<Bill/>}/>
-        <Route path="/finance" element={<FinanceDash/>}></Route>
-        <Route path="/:id" element={<ProfitDetails/>}></Route>
         <Route path="/CreateBill" element={<CreateBill/>}/>
         <Route path="/otherExpense/update/:id" element={<UpdateOther/>}/>
-
-        
-      </Routes>
-
         <Route path="/bill/update/:id" element={<UpdateBill/>}/>
         <Route path="/bill/preview/:id" element={<BillPreview/>}/>
       </Routes>  
     
-  
-  </BrowserRouter>
   </div>
+  </BrowserRouter>
+    
   );
 }
 
