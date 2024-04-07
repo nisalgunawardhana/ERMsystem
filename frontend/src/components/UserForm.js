98df632fa5ce3581b07f4be95adb182edc649c01
Devangap <101203575+Devangap@users.noreply.github.com>
2024-04-07 12:00:07 +0530

    Update UserForm.js

--- frontend/src/components/UserForm.js
+++ frontend/src/components/UserForm.js
@@ -1,52 +1,52 @@
-import { useState } from "react"
+// import { useState } from "react"
 
-const UserForm = () => {
+// const UserForm = () => {
 
-    const [first_name, setFirst_name] = useState('')
-    const [last_name, setLast_name] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+//     const [first_name, setFirst_name] = useState('')
+//     const [last_name, setLast_name] = useState('')
+//     const [email, setEmail] = useState('')
+//     const [password, setPassword] = useState('')
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+//     const handleSubmit = async (e) => {
+//         e.preventDefault()
 
-        const user = {first_name, last_name, email, password}
+//         const user = {first_name, last_name, email, password}
 
-        const response = await fetch('')
-    }
-    return(
-        <form className="create" onSubmit={handleSubmit}>
-            <h3>Add New System User</h3>
+//         const response = await fetch('')
+//     }
+//     return(
+//         <form className="create" onSubmit={handleSubmit}>
+//             <h3>Add New System User</h3>
 
-            <label>First Name : </label>
-            <input 
-                type="text"
-                onChange={(e) => setFirst_name(e.target.value)}
-                value={first_name}
-            />
+//             <label>First Name : </label>
+//             <input 
+//                 type="text"
+//                 onChange={(e) => setFirst_name(e.target.value)}
+//                 value={first_name}
+//             />
 
-            <label>Last Name : </label>
-            <input 
-                type="text"
-                onChange={(e) => setLast_name(e.target.value)}
-                value={last_name}
-            />
+//             <label>Last Name : </label>
+//             <input 
+//                 type="text"
+//                 onChange={(e) => setLast_name(e.target.value)}
+//                 value={last_name}
+//             />
 
-            <label>Email : </label>
-            <input 
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-            />
+//             <label>Email : </label>
+//             <input 
+//                 type="email"
+//                 onChange={(e) => setEmail(e.target.value)}
+//                 value={email}
+//             />
 
-            <label>Password : </label>
-            <input 
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-            />
+//             <label>Password : </label>
+//             <input 
+//                 type="password"
+//                 onChange={(e) => setPassword(e.target.value)}
+//                 value={password}
+//             />
 
-            <button>Add User</button>
-        </form>
-    )
-}
+//             <button>Add User</button>
+//         </form>
+//     )
+// }
