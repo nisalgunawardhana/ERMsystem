7b5edad7a3c95dd20d99dd4491de87e74da1b24f
Devangap <101203575+Devangap@users.noreply.github.com>
2024-04-07 09:39:01 +0530

    Update useSignup.js

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -10,7 +10,7 @@ export const useSignup = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('api/systemUser/add', {
+    const response = await fetch('/user/signup', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ first_name, last_name, email, password, userRole })
