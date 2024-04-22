3b59c9171cb37859485c86398a06e560d10c304e
Navod <127721303+IT22061102@users.noreply.github.com>
2024-04-22 06:56:44 +0530

    navod updates

--- frontend/src/components/employeeComponent/attendancelist.js
+++ frontend/src/components/employeeComponent/attendancelist.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Layout from '../Layout';
+import employeecomponent from './employee'
 
-export default function Trainee() {
+export default function Employee() {
     const [employeeIds, setEmployeeIds] = useState([]);
     const [selectedDate, setSelectedDate] = useState('');
     const [link, setLink] = useState('http://localhost:3000/dashboard/employee/attendence/form');
@@ -51,7 +52,7 @@ export default function Trainee() {
                                     <label htmlFor="dateInput" className="form-label">Select Date:</label>
                                     <input type="date" id="dateInput" className="form-control" value={selectedDate} onChange={handleDateChange} />
                                 </div>
-                                <button className="btn btn-primary" onClick={fetchEmployeeIds}>Show Employee Attendance</button>
+                                <button  className="btn btn-outline-primary" style={{ margin: '0 5px' }} onClick={fetchEmployeeIds}>Show Employee Attendance</button>
                             </div>
                         </div>
                     </div>
@@ -75,13 +76,13 @@ export default function Trainee() {
 
                 <div className="card m-3">
                     <div className="card-body">
-                        <p className="card-text">This is the Employee Taken Link</p>
+                        <h3 className="card-text">This is the Attendance Taken Link</h3>
                     </div>
                     <div className="card-footer">
                         <div className="input-group">
                             <input type="text" className="form-control" value={link} readOnly />
                             <div className="input-group-append">
-                                <button className="btn btn-primary" onClick={copyLink}>Copy Link</button>
+                                <button className="btn btn-outline-secondary" style={{ margin: '0 5px' }} onClick={copyLink}>Copy Link</button>
                             </div>
                         </div>
                     </div>
